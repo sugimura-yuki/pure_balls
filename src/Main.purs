0af853f720c240891c6847efddcbe9201cdd67c8
sugimura.yuki <sgmryk@gmail.com>
2021-06-11 13:04:57 +0000

    リストでの処理に変更

--- src/Main.purs
+++ src/Main.purs
@@ -2,20 +2,20 @@ module Main(main) where
 
 import Prelude
 
+import Ball (Ball)
 import Ball as Ball
-import Control.Apply (lift2)
-import Control.Monad.State (State, StateT, execState, execStateT, gets, modify)
+import Control.Monad.State (State)
+import Control.Monad.State as State
 import Data.DateTime (diff)
 import Data.DateTime.Instant (Instant, toDateTime)
 import Data.Foldable (for_)
-import Data.List.Lazy (drop, length, replicateM, zip, (..))
-import Data.Map as Map
+import Data.List.Lazy (List)
+import Data.List.Lazy as List
 import Data.Maybe (Maybe(..))
-import Data.Set (toUnfoldable)
 import Data.Time.Duration (Milliseconds(..))
+import Data.Traversable (for)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
 import Effect.Now (now)
 import FRP.Event as Event
@@ -23,18 +23,17 @@ import FRP.Event.Time (interval)
 import Graphics.Canvas as Canvas
 import Math (pi)
 import Simulate (simulate)
-import UUID (UUID, genUUID)
 
 main :: Effect Unit
 main = Canvas.getCanvasElementById "canvas" >>= case _ of
   Nothing -> log "no canvas found"
   Just canvas -> do
     let
-      width = 300.0
-      height = 300.0
+      width = 500.0
+      height = 500.0
     Canvas.setCanvasWidth canvas width
     Canvas.setCanvasHeight canvas height
-    balls <- execStateT (replicateM 15 createBall) Map.empty
+    balls <- List.replicateM 20 Ball.randomBall
     last <- now
     _ <- simulate {
       init : {balls,last},
@@ -47,48 +46,41 @@ main = Canvas.getCanvasElementById "canvas" >>= case _ of
 
 
 type Member = {
-  balls :: Map.Map UUID Ball.Ball,
+  balls :: List Ball,
   last :: Instant
 }
 
-createBall :: StateT (Map.Map UUID Ball.Ball) Effect Unit
-createBall = do
-  uuid <- liftEffect genUUID
-  ball <- liftEffect Ball.randomBall
-  _ <- modify $ Map.insert uuid ball
-  pure unit
-
 _update :: {width :: Number , height :: Number} -> Event.Event (Member -> Member)
-_update wh = (execState <<< hdl) <$> interval 15 where
+_update wh = ( State.execState <<< hdl) <$> interval 15 where
   hdl :: Instant -> State Member Unit
   hdl now = do
     -- 前回処理からの経過時間
-    Milliseconds dt <- gets \m -> diff (toDateTime now) (toDateTime m.last)
+    Milliseconds dt <- State.gets \m -> diff (toDateTime now) (toDateTime m.last)
     -- 現在時刻を保持
-    _ <- modify \b -> b{last = now}
+    _ <-  State.modify \b -> b{last = now}
     -- 反射処理
-    _ <- modify \b -> b{balls = (Ball.reflect dt wh) <$> b.balls}
+    _ <-  State.modify \b -> b{balls = (Ball.reflect dt wh) <$> b.balls}
     -- 衝突処理
-    _ <- modify \b -> b{balls = execState (collision dt) b.balls}
+    _ <-  State.modify \b -> b{balls = collision dt b.balls}
     -- 移動処理
-    _ <- modify \b -> b{balls = (Ball.move dt) <$> b.balls}
+    _ <-  State.modify \b -> b{balls = (Ball.move dt) <$> b.balls}
     pure unit
     where
-    -- すべての組み合わせに対して衝突処理をおこなう
-    collision :: Number -> State (Map.Map UUID Ball.Ball) Unit
-    collision dt = do
-      balls <- gets (toUnfoldable <<< Map.keys)
-      for_ (zip (1 .. length balls) balls) \(Tuple n k1) -> 
-        for_ (drop n balls) \k2 -> do
-          mv1 <- gets (Map.lookup k1)
-          mv2 <- gets (Map.lookup k2)
-          case lift2 Tuple mv1 mv2 of
-            Nothing -> pure unit
-            Just (Tuple v1 v2) -> do
-              let next = Ball.colllisionWith dt {v1,v2}
-              _ <- modify (Map.insert k1 next.v1)
-              _ <- modify (Map.insert k2 next.v2)
-              pure unit
+      -- すべての組み合わせに対して衝突処理をおこなう
+      collision :: Number -> List Ball -> List Ball
+      collision dt xs = case List.uncons xs of
+        Nothing -> mempty
+        Just {head,tail} ->
+          let
+            fn ys = for ys \v2 -> do
+              v1 <- State.get
+              let r = Ball.colllisionWith dt {v1,v2}
+              State.put r.v1
+              pure r.v2
+            -- 他のボールと衝突処理後
+            Tuple tail' head' = State.runState (fn tail) head
+          in
+            List.cons head' (collision dt tail')
 
 _render :: Canvas.CanvasElement -> Member -> Effect Unit
 _render canvas m = do
